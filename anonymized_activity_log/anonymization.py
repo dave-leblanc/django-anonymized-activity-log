41942c07b7115438c6faf5cd1fa69ff02f2b13aa
Dave LeBlanc <dave.leblanc@gmail.com>
2016-11-03 21:35:48 -0300

    formatting

--- anonymized_activity_log/anonymization.py
+++ anonymized_activity_log/anonymization.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 from anonymized_activity_log.middleware import get_encryption_function
 
+
 def anonymize_user(request):
     if getattr(request, 'user', None) and request.user.is_authenticated():
         user_str = str(str(request.user.pk) + settings.SECRET_KEY).encode('utf-8')

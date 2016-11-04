dd1d9f8d89fa1a0dc83e8e9397730fd6539ab587
Dave LeBlanc <dave.leblanc@gmail.com>
2016-11-03 21:35:20 -0300

    new crypto options

--- anonymized_activity_log/crypto.py
+++ anonymized_activity_log/crypto.py
@@ -2,6 +2,19 @@ import hashlib
 
 
 def md5_hexdigest(data):
-    return hashlib.md5(data).hexdigest()
+    return hashlib.md5(data.encode('utf-8')).hexdigest()
 
-# TODO: add more crypto types
+def sha1_hexdigest(data):
+    return hashlib.sha1(data.encode('utf-8')).hexdigest()
+
+def sha224_hexdigest(data):
+    return hashlib.sha224(data.encode('utf-8')).hexdigest()
+
+def sha256_hexdigest(data):
+    return hashlib.sha256(data.encode('utf-8')).hexdigest()
+
+def sha384_hexdigest(data):
+    return hashlib.sha384(data.encode('utf-8')).hexdigest()
+
+def sha512_hexdigest(data):
+    return hashlib.sha512(data.encode('utf-8')).hexdigest()
